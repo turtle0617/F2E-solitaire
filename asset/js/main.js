3b22f59aabddb50c0ebdf325a2ef9adf83ff47ac
張睿峰 <qqaz50313@gmail.com>
2019-08-07 20:17:01 +0800

    [Feature]random column can move mutil card to other column and has rule

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,10 +3,11 @@ main(52);
 function main(cardNum) {
   const randomCardMap = setCardNumber(cardNum);
   const cargGroup = sliceCardToGroup(randomCardMap, 4, 7);
-  const cardsColumns = [...document.querySelectorAll('.randomCards__column')];
+  const randomCardContainer = [...document.querySelectorAll('.randomCards__column')];
   const countStorages = [...document.querySelectorAll('.countStorages__item')];
   const temporaryStorages = [...document.querySelectorAll('.temporaryStorages__item')];
-  cardsColumns.forEach((column, index) => {
+  randomCardContainer.forEach((column, index) => {
+    initColumnByRandomElementEvent(column)
     cargGroup[index].forEach(card => column.appendChild(generateCardElement(card)))
   })
   countStorages.forEach(count => initDropedElementEvent(count, "count"))
@@ -14,8 +15,21 @@ function main(cardNum) {
 }
 
 function dragCard(e) {
+  const cardColumn = this.parentNode;
+  const allCardsInColumn = [...cardColumn.children]
+  const isLastCard = cardColumn.lastChild === this;
+  const isOrder = checkCardOrderUntilEnd(this, allCardsInColumn);
+  if (!isLastCard && !isOrder) {
+    e.preventDefault();
+    return;
+  };
+  const currentCardIndex = allCardsInColumn.findIndex((card) => card === this);
+  const currentCardUntilEndId = allCardsInColumn
+    .filter((card, index) => index >= currentCardIndex)
+    .map(card => getCardId(card))
+    .join(',')
   e.dataTransfer.dropEffect = 'move';
-  e.dataTransfer.setData('text/plain', this.id);
+  e.dataTransfer.setData('text/plain', currentCardUntilEndId);
   this.style.opacity = '0.4';
 }
 
@@ -39,18 +53,35 @@ function initDropedElementEvent(item, status) {
   item.addEventListener('drop', coutElementDrop)
 }
 
+function initColumnByRandomElementEvent(column) {
+  column.addEventListener('dragenter', function(e) {
+    this.classList.add('over');
+  })
+  column.addEventListener('dragleave', function(e) {
+    this.classList.remove('over');
+  })
+  column.addEventListener('dragover', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  })
+  column.addEventListener('drop', columnByRandomElementDrop)
+}
+
 function coutElementDrop(e) {
   e.stopPropagation();
   e.preventDefault();
   this.classList.remove('over');
-  const dropCard = document.getElementById(e.dataTransfer.getData("text"));
+  const dropCardId = e.dataTransfer.getData("text").split(',');
+  if (dropCardId.length > 1) return;
+
+  const dropCard = document.getElementById(dropCardId.join(''));
   const hasCardCount = this.children.length;
   if (hasCardCount) {
     const isSameSuit = checkCardSuit(dropCard, this.children[0]);
-    const order = checkCardOrder(dropCard, this.lastChild)
+    const order = checkCardOrder(dropCard, this.lastChild, "increment")
     if (!isSameSuit || !order) return
   } else {
-    const isAce = parseInt(dropCard.getAttribute('id').replace(/^(\D+)/, "")) === 1;
+    const isAce = getCardNumber(dropCard) === 1;
     if (!isAce) return;
   }
   this.appendChild(dropCard);
@@ -60,10 +91,26 @@ function temporaryElementDrop(e) {
   e.stopPropagation();
   e.preventDefault();
   this.classList.remove('over');
-  const dropCard = document.getElementById(e.dataTransfer.getData("text"));
+  const dropCardId = e.dataTransfer.getData("text").split(',');
+  if (dropCardId.length > 1) return;
+  const dropCard = document.getElementById(dropCardId.join(''));
   const hasCardCount = this.children.length;
   if (hasCardCount) return;
   this.appendChild(dropCard);
+}
+
+function columnByRandomElementDrop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  this.classList.remove('over');
+  const cardGroup = e.dataTransfer.getData("text").split(',');
+  const firtCardInGroup = document.getElementById(cardGroup[0]);
+  const meetRule = compareCardMeetsTheRule(this.lastChild, firtCardInGroup);
+  if (!meetRule) return;
+  cardGroup.forEach(cardId => {
+    const dropCard = document.getElementById(cardId);
+    this.appendChild(dropCard);
+  })
 }
 
 function setCardNumber(cardNum) {
@@ -105,13 +152,57 @@ function generateCardElement(card) {
 }
 
 function checkCardSuit(droppedCard, bottomCard) {
-  const suit = bottomCard.getAttribute('id').replace(/(\d+)/, "");
-  const droppedCard_suit = droppedCard.getAttribute('id').replace(/(\d+)/, "");
+  const suit = getCardSuit(bottomCard);
+  const droppedCard_suit = getCardSuit(droppedCard)
   return suit === droppedCard_suit;
 }
 
-function checkCardOrder(droppedCard, previousCard) {
-  const previousCard_num = parseInt(previousCard.getAttribute('id').replace(/^(\D+)/, ""));
-  const droppedCard_num = parseInt(droppedCard.getAttribute('id').replace(/^(\D+)/, ""));
-  return (droppedCard_num - previousCard_num) === 1;
+function checkCardOrder(droppedCard, previousCard, status) {
+  status = status === "increment" ? 1 : -1;
+  const previousCard_num = getCardNumber(previousCard)
+  const droppedCard_num = getCardNumber(droppedCard)
+  return (droppedCard_num - previousCard_num) === status;
+}
+
+function checkCardOrderUntilEnd(currentCard, allCards) {
+  const currentCardIndex = allCards.findIndex((card) => card === currentCard);
+  const currentCardUntilEnd = allCards.filter((card, index) => index >= currentCardIndex);
+  return currentCardUntilEnd.every((card, index, cardGroup) => {
+    const isLast = index === (cardGroup.length - 1)
+    if (isLast) return true;
+    return compareCardMeetsTheRule(card,cardGroup[index + 1])
+    // const currentSuit = converCardSuitToColor(card);
+    // const nextSuit = converCardSuitToColor(cardGroup[index + 1]);
+    // const currentNum = getCardNumber(card);
+    // const nextNum = getCardNumber(cardGroup[index + 1]);
+    // if (currentSuit !== nextSuit && (currentNum - nextNum) === 1)
+    //   // if ((currentNum - nextNum) === -1)
+    //   return true;
+    // return false
+  })
+}
+
+function compareCardMeetsTheRule(currentCard, nextCard) {
+  const currentSuit = converCardSuitToColor(currentCard);
+  const nextSuit = converCardSuitToColor(nextCard);
+  const currentNum = getCardNumber(currentCard);
+  const nextNum = getCardNumber(nextCard);
+  return currentSuit !== nextSuit && (currentNum - nextNum) === 1
+}
+
+function converCardSuitToColor(card) {
+  const suit = getCardSuit(card);
+  return suit === '愛心' || suit === '方塊' ? 'red' : 'black'
+}
+
+function getCardSuit(card) {
+  return card.getAttribute('id').replace(/(\d+)/, "")
+}
+
+function getCardNumber(card) {
+  return parseInt(card.getAttribute('id').replace(/^(\D+)/, ""))
+}
+
+function getCardId(card) {
+  return card.getAttribute('id')
 }
