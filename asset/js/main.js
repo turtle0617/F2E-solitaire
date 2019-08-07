277f1625bcb1f2f9e753c6f7844c250c9aac586d
張睿峰 <qqaz50313@gmail.com>
2019-08-07 13:20:17 +0800

    [Feature]temporary and count drop card rule

--- asset/js/main.js
+++ asset/js/main.js
@@ -7,10 +7,10 @@ function main(cardNum) {
   const countStorages = [...document.querySelectorAll('.countStorages__item')];
   const temporaryStorages = [...document.querySelectorAll('.temporaryStorages__item')];
   cardsColumns.forEach((column, index) => {
-    cargGroup[index].forEach(card =>column.appendChild(generateCardElement(card)))
+    cargGroup[index].forEach(card => column.appendChild(generateCardElement(card)))
   })
-  countStorages.forEach(count=>initDropedItemEvent(count))
-  temporaryStorages.forEach(count=>initDropedItemEvent(count))
+  countStorages.forEach(count => initDropedElementEvent(count, "count"))
+  temporaryStorages.forEach(temporary => initDropedElementEvent(temporary, "temporary"))
 }
 
 function dragCard(e) {
@@ -18,13 +18,12 @@ function dragCard(e) {
   e.dataTransfer.setData('text/plain', this.id);
   this.style.opacity = '0.4';
 }
+
 function dropCard(e) {
-  console.log('dropCard',e.target);
   this.style.opacity = '1';
 }
 
-function initDropedItemEvent(item) {
-  // console.log(item);
+function initDropedElementEvent(item, status) {
   item.addEventListener('dragenter', function(e) {
     this.classList.add('over');
   })
@@ -35,13 +34,36 @@ function initDropedItemEvent(item) {
     e.stopPropagation();
     e.preventDefault();
   })
-  item.addEventListener('drop', function(e) {
-    e.stopPropagation();
-    e.preventDefault();
-    this.classList.remove('over');
-    const dropCard = document.getElementById(e.dataTransfer.getData("text"))
-    this.appendChild(dropCard);
-  })
+  if (status === "temporary")
+    return item.addEventListener('drop', temporaryElementDrop)
+  item.addEventListener('drop', coutElementDrop)
+}
+
+function coutElementDrop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  this.classList.remove('over');
+  const dropCard = document.getElementById(e.dataTransfer.getData("text"));
+  const hasCardCount = this.children.length;
+  if (hasCardCount) {
+    const isSameSuit = checkCardSuit(dropCard, this.children[0]);
+    const order = checkCardOrder(dropCard, this.lastChild)
+    if (!isSameSuit || !order) return
+  } else {
+    const isAce = parseInt(dropCard.getAttribute('id').replace(/^(\D+)/, "")) === 1;
+    if (!isAce) return;
+  }
+  this.appendChild(dropCard);
+}
+
+function temporaryElementDrop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  this.classList.remove('over');
+  const dropCard = document.getElementById(e.dataTransfer.getData("text"));
+  const hasCardCount = this.children.length;
+  if (hasCardCount) return;
+  this.appendChild(dropCard);
 }
 
 function setCardNumber(cardNum) {
@@ -73,11 +95,23 @@ function generateCardElement(card) {
   const cardElement = document.createElement('div');
   const img = document.createElement('img');
   cardElement.setAttribute('class', 'card');
-  cardElement.setAttribute('id',card);
+  cardElement.setAttribute('id', card);
   cardElement.setAttribute('draggable', 'true');
   img.setAttribute('src', `/asset/image/poke/${card}.png`);
   cardElement.appendChild(img);
-  cardElement.addEventListener('dragstart',dragCard);
-  cardElement.addEventListener('dragend',dropCard);
+  cardElement.addEventListener('dragstart', dragCard);
+  cardElement.addEventListener('dragend', dropCard);
   return cardElement
+}
+
+function checkCardSuit(droppedCard, bottomCard) {
+  const suit = bottomCard.getAttribute('id').replace(/(\d+)/, "");
+  const droppedCard_suit = droppedCard.getAttribute('id').replace(/(\d+)/, "");
+  return suit === droppedCard_suit;
+}
+
+function checkCardOrder(droppedCard, previousCard) {
+  const previousCard_num = parseInt(previousCard.getAttribute('id').replace(/^(\D+)/, ""));
+  const droppedCard_num = parseInt(droppedCard.getAttribute('id').replace(/^(\D+)/, ""));
+  return (droppedCard_num - previousCard_num) === 1;
 }
